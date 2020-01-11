940d15a1452f0089004bc9466a790bb24774e0cf
Chris <chrishaining@yahoo.co.uk>
2020-01-11 14:22:40 +0000

    tidies coin_flipper...stores_results

--- coin_flipper_which_stores_results.py
+++ coin_flipper_which_stores_results.py
@@ -1,6 +1,5 @@
 import random
 from matplotlib import pyplot as plt
-# import numpy as np
 
 #list of the two possible outcomes of flipping a coin
 possibilities = ["Heads", "Tails"]
@@ -27,8 +26,8 @@ def play_multiple_times(number):
     play()
   print("The full list of results is {}.".format(outcomes))
 
-play_multiple_times(100)
 
+# work out the proportion of heads and tails
 total_heads = outcomes.count("Heads")
 total_tails = outcomes.count("Tails")
 total_outcomes = [total_heads, total_tails]
@@ -40,10 +39,11 @@ def calculate_probability():
   print("Probability of heads is: {}.".format(heads_probability))
   print("Probability of tails is: {}.".format(tails_probability))
 
+# call the functions
+play_multiple_times(100)
 calculate_probability()
 
-# add probability the pie chart
-
+# display in pie chart form
 plt.pie(total_outcomes, autopct='%0.2f%%')
 plt.legend(names)
 plt.show()
